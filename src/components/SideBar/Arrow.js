115626d3d5dbaa66979771697403ae0dd2acdbd3
lexiaoau <46389469+lexiaoau@users.noreply.github.com>
2019-06-25 16:53:27 +1000

    Create Arrow.js

---
+++ src/components/SideBar/Arrow.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Arrow = (props) => {
+    return (
+        // <b className="arrowUp"></b>
+        <b className={props.arrowStyle}></b>
+    );
+}
+
+export default Arrow;
